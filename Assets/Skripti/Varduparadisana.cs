ed594bef1709d8997dcac78a04a305420e7b76ab
Richards Griezitis <ricis1818@gmail.com>
2021-03-08 13:38:59 +0200

    Pirmas stundas beigas

---
+++ Assets/Skripti/Varduparadisana.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Varduparadisana : MonoBehaviour { 
+
+
+    public string teksts;
+    public GameObject ievadesLauks;
+    public GameObject tekstaAttelosana;
+
+
+    public void uzglabatTekstu()
+    {
+        teksts = ievadesLauks.GetComponent<Text>().text;
+        tekstaAttelosana.GetComponent<Text>().text = "SVEIKS " + teksts.ToUpper() + "!";
+    }
+
+}
